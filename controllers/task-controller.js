759d2d307bfc81d6c65c5b59ee2f53f518247bab
Ehtsham Aziz <ehttimalik@hotmail.com>
2024-03-29 00:12:31 +0500

    Added query params for all tasks in controller

--- controllers/task-controller.js
+++ controllers/task-controller.js
@@ -386,19 +386,189 @@ const DeleteTasks = async (req, res, next) => {
 
 const SendAllTasks=async (req,res,next)=>{
   try{
+    let tasks;
 
-    console.log("aaaaaaaaaaa")
-    console.log(req.query.params);
-    const task= await Tasks.find({taskType:req.query.params});
-    console.log(task)
-    return res.status(200).json(task)
+    if(req.query.params==='birdRecord'|| 'earlyFeeding'){
+     tasks = await Tasks.find({ 
+            eggBirdId: { $exists: true, $ne: null } 
+})
+    .populate({
+    path: "eggBirdId",
+    select: "birdId birdSpecie birdName eggID cageNumber farm birdId gender birthDate exactBirthDate status source price imageURL couple ringNumber ",
+     populate: [
+    {
+      path: "eggID",
+      select: "clutch eggNumber eggsLaidDate",
+      populate: {
+        path: "clutch",
+        select: "couple clutchNumber",
+        populate: {
+        path: "couple",
+        select: "coupleId specie",
+        populate: {
+          path: "specie",
+          select: "addRingAfter incubation addRingAfter startFeedingAfter"
+        }
+      }
+      }
+    },
+    {
+      path: "farm", 
+      select: "farmName _id farmType" 
+    },
+       {
+      path: "birdSpecie", 
+      select: "_id name specieType" 
+    },
+      {
+      path: "couple", 
+      select: "_id coupleId" 
+    }
+  ]
+ 
+  })
+  .populate({
+    path: "eggBirdId.farm",
+    select: "_id farmType farmName"
+  });
+  console.log(tasks)
+  }
+  else if(req.query.params==='treatment'){
+  console.log("Medical task");
+  tasks = await Tasks.find({ 
+              $or: [
+      { treatmentId: { $exists: true, $ne: null } },
+      { birdId: { $exists: true, $ne: null } },
+      { coupleId: { $exists: true, $ne: null } }
+    ]
+ // This condition checks for tasks where birdId exists and is not null.
+})
+
+for (let task of tasks) {
+  // Initialize an array to hold populate options
+  const populateOptions = [];
+  
+  // Check if treatmentId exists and push its populate option
+  if (task.treatmentId) {
+    populateOptions.push({
+      path: 'treatmentId',
+      select: 'treatmentStartDate treatmentName',
+      // Add more populate options here if needed
+    });
+
+    if (task.birdId) {
+    populateOptions.push({
+      path: 'birdId',
+      // Specify select fields if needed, e.g., 'name age'
+    });
+  }
+
+  // Check if coupleId exists and push its populate option
+  if (task.coupleId) {
+    populateOptions.push({
+      path: 'coupleId',
+      // Specify select fields if needed
+    });
+  }
+  }
 
 
+  if (populateOptions.length > 0) {
+  console.log("Medical task222");
+
+    await Tasks.populate(task, populateOptions);
+  }
+}
+
+
+  }
+  else if(req.query.params==='nutrition'){
+  console.log("Nutrition task");
+  tasks = await Tasks.find({ user: req.params.id ,
+              $or: [
+      { nutritionId: { $exists: true, $ne: null } },
+      // { birdId: { $exists: true, $ne: null } },
+      // { coupleId: { $exists: true, $ne: null } }
+    ]
+ // This condition checks for tasks where birdId exists and is not null.
+})
+console.log(tasks);
+
+for (let task of tasks) {
+ 
+  // Initialize an array to hold populate options
+  const populateOptions = [];
+  
+  // Check if treatmentId exists and push its populate option
+  if (task.nutritionId) {
+  console.log("b")
+
+    populateOptions.push({
+      path: 'nutritionId',
+      // select: 'Date mealType',
+      // Add more populate options here if needed
+    });
+
+    if (task.birdId) {
+    populateOptions.push({
+      path: 'birdId',
+      // Specify select fields if needed, e.g., 'name age'
+    });
+  }
+
+  // Check if coupleId exists and push its populate option
+  if (task.coupleId) {
+    populateOptions.push({
+      path: 'coupleId',
+      // Specify select fields if needed
+    });
+  }
+  }
+
+
+  if (populateOptions.length > 0) {
+
+    await Tasks.populate(task, populateOptions);
+    console.log(task);
+  }
+}
+
+
+  }
+  
+  else  if(req.query.params==='hatching'||'fertility'){
+     tasks = await Tasks.find({ 
+              $or: [
+      { eggId: { $exists: true, $ne: null } }],} )
+    .populate({
+    path: "eggId",
+    select: "clutch eggsLaidDate status eggNumber",
+    populate: {
+      path: "clutch",
+      select: "incubationStartDate couple clutchNumber",
+      populate: {
+        path: "couple",
+        select: "coupleId cageNumber specie",
+        populate: {
+          path: "specie",
+          select: "fertilityDays incubation addRingAfter startFeedingAfter"
+        }
+      }
+    }
+  });
+    // console.log(req.query.params);
+    // const tasksType=req.query.params;
+    // const task= await Tasks.find({taskType:tasksType});
+    // console.log(task)
+    return res.status(200).json(tasks)
+
+
+}
   }catch(e){
     next(e);
   }
-
 }
+
 
 
 
