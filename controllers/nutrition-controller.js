92169259a1d2e98ab37b8a82349b0be71b0fc3c6
Ehtsham Aziz <ehttimalik@hotmail.com>
2024-04-17 19:24:33 +0500

    UsersToken correction

--- controllers/nutrition-controller.js
+++ controllers/nutrition-controller.js
@@ -82,8 +82,8 @@ const CreateNutritions = async (req, res, next) => {
 
 async function notificationEndpoint(user,task){
   workers=await Worker.find({user:user._id});
-  const user=await User.findById(user._id);
-  sendCronNotification(user.userToken,task)
+  const users=await User.findById(user._id);
+  sendCronNotification(users.userToken,task)
 
 
   for(const worker of workers){
