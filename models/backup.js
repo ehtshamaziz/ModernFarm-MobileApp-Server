54550109a546f45b7acaff9819b35c305567d0f5
Ehtsham Aziz <ehttimalik@hotmail.com>
2024-08-03 15:21:34 +0500

    Added model for backup2

--- models/backup.js
+++ models/backup.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
 const backupSchema = new Schema({
-  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   backupUrl: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
 });
