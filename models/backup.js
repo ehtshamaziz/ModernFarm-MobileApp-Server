56d076ddae69d78c736ed76df071e48c695e70cc
Ehtsham Aziz <ehttimalik@hotmail.com>
2024-08-03 15:24:39 +0500

    Added model for backup5

--- models/backup.js
+++ models/backup.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const backupSchema = new Schema({
+const backupSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   backupUrl: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
