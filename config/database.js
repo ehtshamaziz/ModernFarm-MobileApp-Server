d8d8c26265dda2cbbb01782b3a1a798fa8210101
Ehtsham Aziz <ehttimalik@hotmail.com>
2024-08-08 08:57:27 +0500

    Added email different6

--- config/database.js
+++ config/database.js
@@ -2,9 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const url=`mongodb+srv://Humza1011:Humza118056@cluster1.bgb1fvt.mongodb.net/modernFarm`
 
-    await mongoose.connect(url);
+    await mongoose.connect(process.env.MONGODB_URL);
     console.log("Connected to MongoDB...");
   } catch (err) {
     console.error("Could not connect to MongoDB...", err);
