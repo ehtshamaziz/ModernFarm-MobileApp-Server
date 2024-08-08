477f7b2fc0fc01d7ba3592a7c1badf6ffab333b9
Ehtsham Aziz <ehttimalik@hotmail.com>
2024-08-08 07:18:19 +0500

    testing config

--- config/database.js
+++ config/database.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL);
+    const dbURL = 'mongodb+srv://Humza1011:Humza118056@cluster1.bgb1fvt.mongodb.net/modernFarm?retryWrites=true&w=majority';
+    await mongoose.connect(dbURL);
     console.log("Connected to MongoDB...");
   } catch (err) {
     console.error("Could not connect to MongoDB...", err);
